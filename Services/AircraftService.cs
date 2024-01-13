8d4e68338bc04261d91b0f016eacd86cf8b00009
Thathsara <thathsara@wireapps.co.uk>
2024-01-13 23:08:52 +0530

    await create aircraft

--- Services/AircraftService.cs
+++ Services/AircraftService.cs
@@ -93,7 +93,7 @@ namespace Raythos_Aerospace.Services
             };
 
             _context.Orders.Add(order);
-            _context.SaveChangesAsync();
+            await git _context.SaveChangesAsync();
         }
 
         public async Task<bool> EditAircraftAsync(AircraftEntity updatedAircraft)
